4e4c1218b48e2777181d4bb7b2bf7f5d0cfd00d3
shrutiiiiin <shrutinaik2317@gmail.com>
2023-12-27 23:20:53 +0530

    inital commit#1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(bmiCalculator());
 }
 
+class bmiCalculator extends StatelessWidget {
+  const bmiCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
