b831f950ebcbcd04dbba1354fb385e8cf3319faf
shrutiiiiin <shrutinaik2317@gmail.com>
2023-12-28 17:55:28 +0530

    intial commit#4

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,65 +19,54 @@ class _InputPageState extends State<InputPage> {
           Row(
             children: [
               Expanded(
-                child: Container(
-                  height: 200,
-                  width: 180,
-                  margin: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                child: ReusableCard(
+                  color: Color(0xFF1D1E33),
                 ),
               ),
               Expanded(
-                  child: Container(
-                height: 200,
-                width: 180,
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
+                child: ReusableCard(
                   color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10),
                 ),
-              ))
+              ),
             ],
           ),
           Expanded(
-            child: Container(
-              height: 200,
-              width: 500,
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10),
-              ),
+            child: ReusableCard(
+              color: Color(0xFF1D1E33),
             ),
           ),
           Row(
             children: [
               Expanded(
-                child: Container(
-                  height: 200,
-                  width: 180,
-                  margin: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                child: ReusableCard(
+                  color: Color(0xFF1D1E33),
                 ),
               ),
               Expanded(
-                  child: Container(
-                height: 200,
-                width: 180,
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
+                child: ReusableCard(
                   color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10),
                 ),
-              ))
+              ),
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.color});
+  Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 180,
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class bmiCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
