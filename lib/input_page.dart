7b8d4d7f17f63922d3a1a820288303dbe6759bbb
shrutiiiiin <shrutinaik2317@gmail.com>
2023-12-28 17:06:56 +0530

    inital commit #4

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,6 +10,19 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Container(
+        height: 200,
+        width: 180,
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
   }
 }
