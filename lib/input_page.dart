12fbaf94d39f5ab7e3b7229e3bf6c3b7bd5eae19
shrutiiiiin <shrutinaik2317@gmail.com>
2023-12-31 12:43:26 +0530

    removed unnec data

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -22,26 +22,6 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
-  // Color malecardColor = inactiveColor;
-  // Color femalecardColor = inactiveColor;
-
-  // void updateColor(Gender gender) {
-  //   if (gender == Gender.male) {
-  //       if (malecardColor == inactiveColor) {
-  //         malecardColor = primaryColor;
-  //         femalecardColor = inactiveColor;
-  //       } else {
-  //         malecardColor = inactiveColor;
-  //       }
-  //     } else {
-  //       if (femalecardColor == inactiveColor) {
-  //         femalecardColor = primaryColor;
-  //         malecardColor = inactiveColor;
-  //       } else {
-  //         femalecardColor = inactiveColor;
-  //       }
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
