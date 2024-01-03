9636702095903ee6bc49071d9c68fea00e9d52c2
shrutiiiiin <shrutinaik2317@gmail.com>
2024-01-03 17:09:58 +0530

    hny #11

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_flutter/constants.dart';
+import 'package:bmi_flutter/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -5,7 +7,24 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Result'),
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text('Your Result'),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kprimaryColor,
+              cardchild: Column(),
+            ),
+          ),
+        ],
       ),
     );
   }
